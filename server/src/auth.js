b99f65316d4471e62ec49ef7b3372a67e115c540
zl0i <dmitri.popov.96@mail.ru>
2021-04-22 21:43:06 +0900

    refractor: auth.js

--- server/src/auth.js
+++ server/src/auth.js
@@ -4,28 +4,37 @@ const User = require('../models/userModel');
 
 const smsApiKey = process.env.SMS_API_KEY || ""
 
+function validatePhone(phone) {
+    if (/^\+?[7][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/.test(phone))
+        return true
+
+    return false
+}
+
+function generateCode() {
+    return Math.round((Math.random() * 8999 + 1000))
+}
+
 module.exports = {
     sendUserCode: async (phone) => {
         if (phone == '+79999999999') {
             await User.updateOne({ phone: phone }, { smsCode: '9674' }, { upsert: true })
         } else {
-            let code = Math.round((Math.random() * 8999 + 1000))
-            console.log("send code sms to:", phone, code)
+            if (!validatePhone(phone))
+                throw new Error('bad number phone')
+
+            let code = generateCode();
             let reply = await axios.get(`https://sms.ru/sms/send?api_id=${smsApiKey}5&to=${phone}&msg=${code}&json=1&from=Azia`);
-            console.log("sms sended")
             if (reply.data.status_code === 100) {
-                console.log("sms sended success")
                 await User.updateOne({ phone: phone }, { smsCode: code }, { upsert: true })
             } else {
                 throw new Error('sms not send')
             }
         }
     },
-    checkUserCode: async (phone, code) => {
-        console.log("task code recived")
+    checkUserCode: async (phone, code) => {       
         let user = await User.findOne({ phone: phone });
         if (user.smsCode === code && code !== '') {
-            console.log('code right')
             user.smsCode = '';
             let token = uuidv4();
             user.createToken = new Date();
@@ -37,12 +46,12 @@ module.exports = {
         }
     },
     checkUserToken: async (phone, token) => {
-        let user = await User.findOne({ phone: phone }, { phone: 1, token: 1 });       
+        let user = await User.findOne({ phone: phone }, { phone: 1, token: 1 });
         if (token == user.token) {
             user.createToken = new Date();
             user.token = uuidv4();
-            user.save();          
-            return user.token;  
+            user.save();
+            return user.token;
         } else {
             throw new Error('token is bad')
         }
