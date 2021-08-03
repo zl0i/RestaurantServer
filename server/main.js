767246344d1934f4620e00c5562a0e88ec05c66b
zl0i <dmitri.popov.96@mail.ru>
2021-08-03 22:17:05 +0900

    fix: enabled cors

--- server/main.js
+++ server/main.js
@@ -19,6 +19,11 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/restaurant/api/users', require('./routers/userRouter'));
 app.use('/restaurant/api/orders', require('./routers/orderRouter'));
 app.use('/restaurant/api/shops', require('./routers/shopRouter'));
