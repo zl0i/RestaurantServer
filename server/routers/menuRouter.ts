2221b30a2e12bb8ba020d9f19bd331d436fc1c32
zl0i <dmitri.popov.96@mail.ru>
2021-08-28 00:33:55 +0900

    feat: images in minio

--- server/routers/menuRouter.ts
+++ server/routers/menuRouter.ts
@@ -4,6 +4,8 @@ import Menu, { MenuStatus } from '../entity/menu';
 import { body } from '../middleware/schemaChecker';
 import MenuCategory from '../entity/menu_category';
 import { cache } from '../middleware/cacheMiddleware';
+import ObjectStorage from '../src/storage'
+import { UploadedFile } from 'express-fileupload';
 
 const router = express.Router();
 
@@ -26,7 +28,7 @@ router.get('/', [scopeValidator('menu:get'), cache(1800)], async (req: express.R
 
 router.post('/',
     [
-        body({ id_category: Number, name: String, cost: Number, icon: String, description: String }),
+        body({ id_category: String, name: String, cost: String, description: String }),
         scopeValidator('menu:create')
     ],
     async (req: express.Request, res: express.Response) => {
@@ -34,9 +36,11 @@ router.post('/',
             const category = await MenuCategory.findOne({ id: req.body.id_category })
             if (category) {
                 const item = new Menu()
+                if (!!req.files?.icon) {
+                    item.icon = await ObjectStorage.uploadImage(req.files.icon as UploadedFile)
+                }
                 item.name = req.body.name
                 item.cost = req.body.cost
-                item.icon = req.body.icon
                 item.id_category = category.id
                 item.id_point = category.id_point
                 item.description = req.body.description
@@ -57,6 +61,7 @@ router.post('/',
     }
 )
 
+//TO DO patch image
 router.patch('/:id',
     [
         body({ id_category: Number, name: String, cost: Number, icon: String, description: String }),
@@ -90,6 +95,7 @@ router.patch('/:id',
     }
 )
 
+//TO DO delete image
 router.delete('/:id', [scopeValidator('menu:delete')], async (req: express.Request, res: express.Response) => {
     try {
         await Menu.delete({ id: Number(req.params.id) })
