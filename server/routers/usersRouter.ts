4e98cf17c17ca15e347d690af00cf4b457a00c7a
zl0i <dmitri.popov.96@mail.ru>
2021-09-24 11:43:37 +0900

    feat: delete user method

--- server/routers/usersRouter.ts
+++ server/routers/usersRouter.ts
@@ -6,6 +6,9 @@ import bcrypt from 'bcryptjs'
 import PermissionsBuilder, { UserRoles } from '../lib/permissionsBuilder';
 import { cache } from '../middleware/cacheMiddleware';
 import { In } from 'typeorm';
+import { Tokens } from '../entity/tokens';
+import { token_permissions } from '../entity/token_permissions';
+import { user_permissions } from '../entity/user_permissions';
 
 const router = express.Router();
 
@@ -15,7 +18,18 @@ router.get('/', [scopeValidator('users:read'), cache(600)], async (req: express.
     if (req.context?.condition.value.length > 0) {
       condition[req.context?.condition?.key] = In(req.context?.condition.value)
     }
-    res.status(200).json(await Users.find(condition));
+    const users: any = await Users.find(condition)
+    for (const user of users) {
+      delete user.sms_code
+      delete user.sms_code_expired_at
+      delete user.password
+      const permissions = await user_permissions.find({ id_user: user.id })
+      user.permissions = new Array()
+      for (const perm of permissions) {
+        user.permissions.push(`${perm.resource}:${perm.action}:${perm.scope}`)
+      }
+    }
+    res.status(200).json(users);
   } catch (error) {
     console.log(error)
     res.status(500).end();
@@ -38,5 +52,30 @@ router.post('/', [body({ login: String, password: String }), scopeValidator('use
     res.status(500).end();
   }
 });
+
+router.delete('/:id', [scopeValidator('users:delete')], async (req: express.Request, res: express.Response) => {
+  try {
+    const user_id: number = Number(req.params.id)
+    const user = await Users.findOne({ id: user_id })
+    if (user) {
+      await user.remove()
+      await user_permissions.delete({ id_user: user_id })
+      const token = await Tokens.findOne({ id_user: user_id })
+      await token_permissions.delete({ id_token: token.id })
+      await token.remove()
+      res.json({
+        result: 'ok'
+      })
+    } else {
+      res.status(400).json({
+        result: 'error',
+        message: 'User not found'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).end();
+  }
+})
 
 export default router
