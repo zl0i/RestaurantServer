48638f869c85cc3009031fd4323fa0f353e6d05f
zl0i <dmitri.popov.96@mail.ru>
2021-11-16 22:15:50 +0900

    feat: if db error then http code is 500

--- server/lib/httpErrorHandler.ts
+++ server/lib/httpErrorHandler.ts
@@ -1,17 +1,26 @@
 import express from "express";
+import { TypeORMError } from "typeorm";
 import HttpError from "./httpError";
+
+const IS_TEST = process.env['NODE_ENV'] == 'test'
 
 export default class HttpErrorHandler {
     static handle(error: Error, res: express.Response) {
         console.error(error)
         if (error instanceof HttpError) {
             res.status(error.status).json({
-                error: 'error',
+                result: 'error',
                 mesage: error.message
             });
+        } else if (error instanceof TypeORMError) {
+            res.status(500).json({
+                result: 'error',
+                message: IS_TEST ? 'DB error' : error.message
+            })
         } else {
             res.status(500).json({
-                message: error.message
+                result: 'error',
+                message: IS_TEST ? 'Internal Server Error' : error.message
             })
         }
     }
