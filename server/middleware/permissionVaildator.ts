16160bed61192c9b7286593023b29ed1ff9997c6
zl0i <dmitri.popov.96@mail.ru>
2021-11-20 00:14:58 +0900

    refractor: permissionValidator.ts

--- server/middleware/permissionVaildator.ts
+++ server/middleware/permissionVaildator.ts
@@ -3,6 +3,8 @@ import { MoreThan } from 'typeorm'
 import { Tokens } from '../entity/tokens.entity'
 import { token_permissions } from '../entity/token_permissions.entity'
 import { Users } from '../entity/user.entity'
+import { ForbiddenError, UnauthorizedError } from '../lib/errors'
+import HttpErrorHandler from '../lib/httpErrorHandler'
 import { Actions, Resources, Scopes } from '../lib/permissionsBuilder'
 import { getCache, setCache } from './cacheMiddleware'
 import { ICondition } from './scopes/basicScope'
@@ -22,56 +24,67 @@ declare global {
     }
 }
 
+async function fromCache(token: string) {
+    const key = `permissions_token_${token}`
+    let data = await getCache(key)
+    return JSON.parse(data)
+}
+
+async function saveToCache(token: string, user: Users, id_token: number) {
+    const key = `permissions_token_${token}`
+    setCache(key, JSON.stringify({
+        user: user,
+        permissions: await token_permissions.find({ id_token })
+    }), 1800)
+}
+
+function findPermission(permissions: token_permissions[], resource: Resources, action: Actions) {
+    return Array<token_permissions>(...permissions).find((value) => {
+        if (value.resource == resource && value.action == action)
+            return true
+    })
+}
+
+async function getPermissions(str_token: string, resource: Resources, action: Actions) {
+    const token = await Tokens.findOne({ token: str_token, expired_at: MoreThan(new Date()) })
+
+    if (!token)
+        throw new UnauthorizedError("Token invalid")
+
+    const data = await fromCache(token.token)
+
+    const user = data ? data.user : await Users.findOne({ id: token?.id_user })
+    const permissions = data ? findPermission(data.permissions, resource, action) : await token_permissions.findOne({ resource: resource, action: action, id_token: token?.id })
+
+    if (!user)
+        throw new UnauthorizedError("User not found")
+
+    saveToCache(token.token, user, token.id)
+
+    return { user, permissions };
+}
+
 
 export default function allow(resource: Resources, action: Actions) {
     return async (req: express.Request, res: express.Response, next: Function) => {
         try {
-            const token = await Tokens.findOne({ token: req.headers.authorization?.split(" ")[1], expired_at: MoreThan(new Date()) })
-            if (!token)
-                return res.status(401).json({ message: "Token invalid" })
+            const { user, permissions } = await getPermissions(req.headers.authorization?.split(" ")[1], resource, action);
 
-            const key_cache = `permissions_token_${token.token}`
-            let data_cache = await getCache(key_cache)
-            let user: Users, permissions: token_permissions;
+            if (!permissions)
+                throw new ForbiddenError('You don\'t have permissions for it')
 
-            if (data_cache) {
-                data_cache = (JSON.parse(data_cache))
-                permissions = Array<token_permissions>(...data_cache.permissions).find((value) => {
-                    if (value.resource == resource && value.action == action)
-                        return true
-                })
-                user = data_cache.user
-            } else {
-                user = await Users.findOne({ id: token?.id_user })
-                if (!user)
-                    return res.status(401).json({ message: "User not found" })
-
-                permissions = await token_permissions.findOne({ resource: resource, action: action, id_token: token?.id })
-
-                setCache(key_cache, JSON.stringify({
-                    user: user,
-                    permissions: await token_permissions.find({ id_token: token?.id })
-                }), 1800)
+            req.context = {
+                permission: `${resource}:${action}:${permissions.scope}`,
+                isOwn: permissions.scope === Scopes.own,
+                isAll: permissions.scope === Scopes.all,
+                user: user
             }
 
-            if (permissions) {
-                req.context = {
-                    permission: `${resource}:${action}:${permissions.scope}`,
-                    isOwn: permissions.scope === Scopes.own,
-                    isAll: permissions.scope === Scopes.all,
-                    user: user
-                }
-
-                const condition = new ScopeCondition(resource, permissions.scope, user)
-                req.context.condition = condition.getCondition()
-
-                next()
-            } else {
-                res.status(403).end()
-            }
+            const condition = new ScopeCondition(resource, permissions.scope, user)
+            req.context.condition = condition.getCondition()
+            next()
         } catch (e) {
-            console.log(e)
-            res.status(500).end()
+            HttpErrorHandler.handle(e, res)
         }
     }
 }
